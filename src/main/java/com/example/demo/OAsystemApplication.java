a0a31bafd73dc3dd10a19d93b28ebe93377f3eb4
LeNSWZ <506927019@qq.com>
2018-09-25 14:22:59 +0800

    项目提交

---
+++ src/main/java/com/example/demo/OAsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAsystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/notice/domain/Notice.java
@@ -0,0 +1,68 @@
+package com.example.demo.notice.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name="t_notice")
+public class Notice {
+	private Long id;
+	private String noticeNumber;
+	private String title;
+	private String type;
+	private String content;
+	private String userId;
+	private Date createTime;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNoticeNumber() {
+		return noticeNumber;
+	}
+	public void setNoticeNumber(String noticeNumber) {
+		this.noticeNumber = noticeNumber;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	@JsonFormat(pattern="yyyy/MM/dd",timezone="GMT+8")
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}	
+}
